944d81560b80160e4bf689a1b5c6efe62862103f
Jeff Matthews <matthews.jeffery@gmail.com>
2021-05-13 10:59:38 -0500

    Replaced Magento w/ Commerce

--- bin/functions.sh
+++ bin/functions.sh
@@ -5,9 +5,9 @@ edit () {
 
   read -p 'Enter your Magento version (ex, 2.4.3): ' VERSION
 
-  cat __output__/admin-schema-transformed.json | version=$VERSION ruby -rjson -e 's = JSON.load($stdin); s["info"]["version"]=ENV["version"]; s["info"]["title"]="Magento Admin REST endpoints - All inclusive"; s["host"]="example.com"; puts JSON.generate s' > __output__/admin-schema-processed-$VERSION.json
-  cat __output__/customer-schema-transformed.json | version=$VERSION ruby -rjson -e 's = JSON.load($stdin); s["info"]["version"]=ENV["version"]; s["info"]["title"]="Magento Customer REST endpoints - All inclusive"; s["host"]="example.com"; puts JSON.generate s' > __output__/customer-schema-processed-$VERSION.json
-  cat __output__/admin-schema-transformed.json | version=$VERSION ruby -rjson -e 's = JSON.load($stdin); s["info"]["version"]=ENV["version"]; s["info"]["title"]="Magento Guest REST endpoints - All inclusive"; s["host"]="example.com"; puts JSON.generate s' > __output__/guest-schema-processed-$VERSION.json
+  cat __output__/admin-schema-transformed.json | version=$VERSION ruby -rjson -e 's = JSON.load($stdin); s["info"]["version"]=ENV["version"]; s["info"]["title"]="Commerce Admin REST endpoints - All inclusive"; s["host"]="example.com"; puts JSON.generate s' > __output__/admin-schema-processed-$VERSION.json
+  cat __output__/customer-schema-transformed.json | version=$VERSION ruby -rjson -e 's = JSON.load($stdin); s["info"]["version"]=ENV["version"]; s["info"]["title"]="Commerce Customer REST endpoints - All inclusive"; s["host"]="example.com"; puts JSON.generate s' > __output__/customer-schema-processed-$VERSION.json
+  cat __output__/guest-schema-transformed.json | version=$VERSION ruby -rjson -e 's = JSON.load($stdin); s["info"]["version"]=ENV["version"]; s["info"]["title"]="Commerce Guest REST endpoints - All inclusive"; s["host"]="example.com"; puts JSON.generate s' > __output__/guest-schema-processed-$VERSION.json
 
   echo 'Done'
 }
